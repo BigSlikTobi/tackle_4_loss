e7f187b9cf2edfed0b50acf10bc8b9c213db2c33
BigSlikTobi <tobias.latta@hotmail.de>
2025-04-25 10:23:59 +0200

    Add new build configurations and resources for Firebase and other dependencies
    - Created new JSON files for build configurations of FirebaseCoreInternal, FirebaseMessaging, nanopb, firebase_messaging, sqflite_darwin, and url_launcher_ios.
    - Each configuration includes settings for Debug, Profile, and Release builds with appropriate paths and identifiers.
    - Updated workspace configuration to include the new Pods project.

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.29.2/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tobiaslatta/Desktop/Private/T4L/tackle_4_loss"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/tobiaslatta/Desktop/Private/T4L/tackle_4_loss/.dart_tool/package_config.json"
